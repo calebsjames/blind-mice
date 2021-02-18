54038bcedce0988003e0e2a88ef3e8c704a844fe
Caleb James <calebsjames@gmail.com>
2021-02-17 19:48:12 -0600

    related customers

--- scripts/EmployeeList.js
+++ scripts/EmployeeList.js
@@ -1,7 +1,9 @@
 //import statements
 import { getComputers, useComputers } from "./ComputerProvider.js";
+import { getCustomers, useCustomers } from "./CustomerProvider.js";
 import { getDepartments, useDepartments } from "./DepartmentProvider.js";
 import { employees } from "./Employee.js";
+import { getEmployeeCustomers, useEmployeeCustomers } from "./EmployeeCustomerProvider.js";
 import { getEmployees, useEmployees } from "./EmployeeProvider.js";
 import { getLocations, useLocations } from "./LocationProvider.js";
 
@@ -19,29 +21,35 @@ export const employeesList = () => {
         .then(getComputers)
         .then(getDepartments)
         .then(getLocations)
+        .then(getCustomers)
+        .then(getEmployeeCustomers)
         .then(() => {
-        //assign the fetch call to a variable
+        //assign the fetch calls to variables
         const employeesArray = useEmployees()
         const computerArray = useComputers()
         const departmentsArray = useDepartments()
         const locationsArray = useLocations()
+        const customers = useCustomers()
+        const customerRelationships = useEmployeeCustomers()
+
         //render to DOM with said variable
-        employeesToDom(employeesArray, computerArray, departmentsArray, locationsArray)
+        employeesToDom(employeesArray, computerArray, departmentsArray, locationsArray, customers, customerRelationships)
         }
     )}
 
 
 
 //export function to put employee slices on DOM
-const employeesToDom = (allEmployees, allComputers, allDepartments, allLocations) => {
-    debugger
+const employeesToDom = (allEmployees, allComputers, allDepartments, allLocations, allCustomers, allCustomerRelationships) => {
+    // debugger
+
     //Iterate through the employees with .map and insert it into innerHTML
     //This will be the main content of the cards
     contentTarget.innerHTML = allEmployees.map(
         //(variable) to reference values in allEmployees and pass to employees()
         (employeeObject) => {
             //declare a new variable and filter allComputers to get comp.id that is equal to employeeObject.computerId
-            const employeeComputer = allComputers.filter(
+            const employeeComputer = allComputers.find(
                 comp => comp.id === employeeObject.computerId)
             
             const employeeDapartment = allDepartments.find(
@@ -51,36 +59,21 @@ const employeesToDom = (allEmployees, allComputers, allDepartments, allLocations
             const employeeLocation = allLocations.find(
                 loc => loc.id === employeeObject.locationId
             )
-                console.log(employeeObject, employeeComputer, employeeDapartment, employeeLocation)
-                return employees(employeeObject, employeeComputer, employeeDapartment, employeeLocation)
+            
+            debugger
+            const relationships = allCustomerRelationships.filter(
+                rel => rel.employeeId === employeeObject.id
+            )
+            console.log(relationships)
+        
+
+            const assignedCustomers = relationships.map(
+                rel => {
+                    return allCustomers.find(customer => customer.id === rel.customerId)
+                })
+                //pass all arguements to employees()
+                return employees(employeeObject, employeeComputer, employeeDapartment, employeeLocation, assignedCustomers)
             }).join("")
 
-            //pass employeeObject and the matching computer to employees()
 
 }
-
-
-
-
-
-
-
-// //export function to put employee slices on DOM
-// const employeesToDom = (allEmployees, allComputers) => {
-//     debugger
-//     //Iterate through the employees with .map and insert it into innerHTML
-//     //This will be the main content of the cards
-//     contentTarget.innerHTML = allEmployees.map(
-//         //(variable) to reference values in allEmployees and pass to employees()
-//         (employeeObject) => {
-//             //declare a new variable and filter allComputers to get comp.id that is equal to employeeObject.computerId
-//             const employeeComputer = allComputers.filter(
-//                 comp => comp.id === employeeObject.computerId)
-
-//                 console.log(employeeObject, employeeComputer)
-//                 return employees(employeeObject, employeeComputer)
-//             }).join("")
-
-//             //pass employeeObject and the matching computer to employees()
-
-// }
